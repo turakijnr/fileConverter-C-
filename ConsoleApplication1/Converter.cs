d3c3a0fda5df12a8041dedaf4f23f377348c9d1b
nuumanBasshir <nuuman.bashir@outsourceglobal.com>
2021-09-08 11:46:19 +0100

    first commit

---
+++ ConsoleApplication1/Converter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    public class Converter
+    {
+        public  string ConverterName;
+        public string Description;
+        public string Converted;
+       
+        public string Mkvconverter(string path)
+        {
+            
+            
+            string target = Path.ChangeExtension(path, ".mkv");/*Convert file format */
+
+            return target  ;
+
+        }
+       public  string VlcConverter(string path)
+       {
+            
+            
+           string target = Path.ChangeExtension(path, ".vlc");/*Convert file format */
+           var converted = target + "_converted";
+           return converted  ;
+
+       }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+             var converter = new Converter();
+
+             converter.Description = "convert to mkv";
+             converter.ConverterName = "vlc";
+             converter.VlcConverter("gfgfggh.png"); 
+             
+             Console.WriteLine(converter);
+
+        }
+    }
+}
+   
